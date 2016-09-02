39a5076fff2a0eda9c73b7ad6e1a3d518c920ef2
lcnajerao <a01630557@itesm.mx>
2016-09-01 21:40:10 -0500

    Delete output.py

--- output.py
+++
@@ -1,9 +0,0 @@
-#output.py With the objective of explain the output use By Luis Najera
-#For www.codewithcharlie.com
-#Begins Variable definition
-integer    = 0
-floating   = 0.0
-character  = 'G'
-string     = "Subscribe Code With Charlie"
-dlist      = [4,"Cool",0.044,True,'Y']
-#End of Variable definition, begin with the code
