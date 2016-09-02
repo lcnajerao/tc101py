4650daf6ce96340037a6cd2b33a60ea94fa8db5a
LuisNajera <luis@MacBook-Pro-de-Luis.local>
2016-09-01 21:38:53 -0500

    Name change to m4...

---
+++ m4output.py
@@ -0,0 +1,19 @@
+#output.py With the objective of explain the output use By Luis Najera
+#For www.codewithcharlie.com
+#Begins Variable definition
+integer    = 4
+floating   = 4.55
+character  = 'G'
+string     = "Subscribe Code With Charlie"
+dlist      = [9,"Cool",0.044,True,'Y']
+dictionary = {"inte":8,"floa":3.089,"char":'L',"strings":"Hallo"}
+#End of Variable definition, begin with the print Code
+print(integer)
+print(floating)
+print(character)
+print(string)
+print(dlist)
+print(dictionary)
+print(dlist[0])
+print(dictionary["inte"])
+print(string,dictionary["floa"])
