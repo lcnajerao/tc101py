820f0a1acc9c722a2ac4eb05b96788b94a0aa247
LuisNajera <luis@MacBook-Pro-de-Luis.local>
2016-09-01 22:44:46 -0500

    Finished the m2 program

---
+++ m2lists.py
@@ -0,0 +1,28 @@
+#m2lists.py With the objective of explain the lists in python use By Luis Najera
+#For www.codewithcharlie.com
+#Begins Variable definition
+#-----[  ]---- The brakets are the definers of the lists
+results = [4,5.6,'l',"Codewithcharlie"] #The list begins with the place 0
+demo    = []
+#Begins user interface part
+print("This program will perform a series of operation with lists")
+print("First Example")
+print(results[0])
+print("Second Example")
+print(results[1])
+print("Third Example")
+print(results[2])
+print("Forth Example")
+print(results[3])
+print("Fifth Example")
+print(results)
+print("Sixth Example")
+print(4 in results)
+print("Seventh Example")
+print(5 in results)
+print("Eighth Example")
+print(results[0:3]) #the function will exclude the upper limit
+print("Ninth Example")
+print(results*3)
+print("Tenth Example")
+print(results[1]*3) 
