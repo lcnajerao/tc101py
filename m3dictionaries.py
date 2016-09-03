8fa9388e0cb61f36173742429d081cf5f8e64c96
LuisNajera <luis@MacBook-Pro-de-Luis.local>
2016-09-03 14:34:27 -0500

    Mastery 3 Ready to publish

---
+++ m3dictionaries.py
@@ -0,0 +1,24 @@
+#m2lists.py With the objective of explain the lists in python use By Luis Najera
+#For www.codewithcharlie.com
+#Begins Variable definition
+#-----{}---- The brakets are the definers of the dictionaries
+result = {"integer":4,"floating":4.44444,"char":'L',"string":"Codewithcharlie","list1":[1,2,3,4]}
+print("This program will perform a series of operation with Dictionaries")
+print("First Example")
+print(result["integer"])
+print("Second Example")
+print(result["floating"])
+print("Third Example")
+print(result["char"])
+print("Forth Example")
+print(result["string"])
+print("Fifth Example")
+print(result["list1"])
+print("Sixth Example")
+print(4 in result)
+print("Seventh Example")
+print(5 in result)
+print("Eighth Example")
+print(result["integer"]*result["floating"])
+print("Ninth Example")
+print(result["list1"]*3)
